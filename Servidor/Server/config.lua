67367c3515920301fc97c1a46306be44ab557ae8
Davi <qrzdavi@icloud.com>
2025-02-10 17:14:57 -0300

    docker compose with files

---
+++ Servidor/Server/config.lua
@@ -0,0 +1,121 @@
+worldType = "pvp"
+hotkeyAimbotEnabled = true
+protectionLevel = 1
+killsToRedSkull = 7
+killsToBlackSkull = 9
+pzLocked = 60000
+removeChargesFromRunes = true
+removeChargesFromPotions = true
+removeWeaponAmmunition = true
+removeWeaponCharges = true
+timeToDecreaseFrags = 3600
+whiteSkullTime = 10 * 60
+stairJumpExhaustion = 2000
+experienceByKillingPlayers = false
+expFromPlayersLevelRange = 75
+
+ip = "127.0.0.1"
+bindOnlyGlobalAddress = false
+loginProtocolPort = 7171
+gameProtocolPort = 7172
+statusProtocolPort = 7171
+maxPlayers = 0
+motd = "Welcome to The Shire Legends Server!"
+onePlayerOnlinePerAccount = true
+allowClones = false
+allowWalkthrough = true
+serverName = "Shire Legends RPG"
+statusTimeout = 5000
+replaceKickOnLogin = true
+maxPacketsPerSecond = 25
+
+deathLosePercent = -1
+
+housePriceEachSQM = 100000
+houseRentPeriod = "never"
+houseOwnedByAccount = false
+houseDoorShowPrice = true
+onlyInvitedCanMoveHouseItems = true
+
+timeBetweenActions = 200
+timeBetweenExActions = 0
+
+mapName = "world"
+mapAuthor = "Nekiro"
+
+marketOfferDuration = 30 * 24 * 60 * 60
+premiumToCreateMarketOffer = false
+checkExpiredMarketOffersEachMinutes = 60
+maxMarketOffersAtATimePerPlayer = 100
+sqlType = "mysql"
+mysqlHost = "127.0.0.1"
+mysqlUser = "root"
+mysqlPass = "marisbleuda"
+mysqlDatabase = "msql"
+mysqlPort = 3306
+mysqlSock = ""
+
+allowChangeOutfit = true
+freePremium = true
+kickIdlePlayerAfterMinutes = 15000
+maxMessageBuffer = 25
+emoteSpells = true
+classicEquipmentSlots = true
+classicAttackSpeed = true
+showScriptsLogInConsole = false
+showOnlineStatusInCharlist = false
+yellMinimumLevel = 2
+yellAlwaysAllowPremium = false
+minimumLevelToSendPrivate = 1
+premiumToSendPrivate = false
+forceMonsterTypesOnLoad = true
+cleanProtectionZones = false
+luaItemDesc = false
+showPlayerLogInConsole = true
+
+vipFreeLimit = 20
+vipPremiumLimit = 100
+depotFreeLimit = 2000
+depotPremiumLimit = 10000
+
+defaultWorldLight = true
+
+serverSaveNotifyMessage = true
+serverSaveNotifyDuration = 5
+serverSaveCleanMap = false
+serverSaveClose = false
+serverSaveShutdown = true
+
+experienceStages = {
+	{ minlevel = 1, maxlevel = 7, multiplier = 7 },
+	{ minlevel = 8, maxlevel = 99, multiplier = 300 },
+	{ minlevel = 100, maxlevel = 199, multiplier = 200 },
+	{ minlevel = 200, maxlevel = 299, multiplier = 200 },
+	{ minlevel = 300, maxlevel = 399, multiplier = 250 },
+	{ minlevel = 400, maxlevel = 499, multiplier = 150 },
+	{ minlevel = 500, multiplier = 3 }
+}
+
+rateExp = 5
+rateSkill = 165
+rateLoot = 15
+rateMagic = 165
+rateSpawn = 1
+
+deSpawnRange = 2
+deSpawnRadius = 50
+removeOnDespawn = true
+walkToSpawnRadius = 15
+
+staminaSystem = true
+
+warnUnsafeScripts = true
+convertUnsafeScripts = true
+
+defaultPriority = "high"
+startupDatabaseOptimization = false
+
+	ownerName = ""
+ownerEmail = ""
+url = "https://otland.net/"
+location = "Sweden"
